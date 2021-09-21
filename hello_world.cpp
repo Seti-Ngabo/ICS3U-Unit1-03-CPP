ca88999131c9f8ed82289e4e01e5aa6c193fc5d2
Ubuntu <ubuntu@ip-172-31-51-232.ec2.internal>
2021-09-21 01:15:16 +0000

    added file

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Seti Ngabo ALL rights reserved
+//
+// Created by Seti Ngabo
+// Created on: Sept 2021
+// This is the "Hello, World!" program, with proper style
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!";
+}
